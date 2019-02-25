5f260241bc71878b32b56031f8e6f7fd6d95b713
mraydin <mhaydin@gmail.com>
2019-02-25 16:58:38 +0300

    Last 7 Days Graph Added

--- weekly.php
+++ weekly.php
@@ -1153,12 +1153,14 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
      function drawVisualization() {
          // Some raw data (not necessarily accurate)
          var data = google.visualization.arrayToDataTable([
-             ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-             ['2004/05',  165,      938,         522,             998,           450,      614.6],
-             ['2005/06',  135,      1120,        599,             1268,          288,      682],
-             ['2006/07',  157,      1167,        587,             807,           397,      623],
-             ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-             ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+             ['Month', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'],
+             ['Pazartesi',  165, 938, 522, 998, 450, 614.6, 40],
+             ['Salı',  135, 1120, 599, 1268, 288, 682, 30],
+             ['Çarşamba',  157, 1167, 587, 807, 397, 623, 20],
+             ['Perşembe',  139, 1110, 615, 968, 215, 609.4 30],
+             ['Cuma',  139, 1110, 615, 968, 215, 609.4, 40],
+             ['Cumartesi',  139, 1110, 615, 968, 215, 609.4, 50],
+             ['Pazar',  136, 691, 629, 1026, 366, 569.6, 60]
          ]);
 
          var options = {
