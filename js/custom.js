ca72072a8df86a41099c507627ee04ef39d89504
mraydin <mhaydin@gmail.com>
2019-02-26 19:21:44 +0300

    Week Graph Customize

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/** ******  left menus  *********************** **/
+/** ******  left menu  *********************** **/
 $(function () {
     $('#sidebar-menu li ul').slideUp();
     $('#sidebar-menu li').removeClass('active');
