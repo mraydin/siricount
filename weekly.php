f8acd7f62d25f09b666c370d61f0ac339d3b841f
mraydin <mhaydin@gmail.com>
2019-02-26 11:09:08 +0300

    Week Graph Customize

--- weekly.php
+++ weekly.php
@@ -1150,7 +1150,7 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
  <script>
 
 
-     console.log ("Pazartesi", updateConfigByMutating()[0].responseJSON);
+     console.log ("Pazartesi", updateConfigByMutating().responseJSON);
 
      function updateConfigByMutating() {
          var myObject = {name: moment().subtract(1, 'days').format('DD.MM.YYYY'), s: "submit"};
