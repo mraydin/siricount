defdd7fc93eb6e9eecdf40377fb572de78ceb23e
Mustafa Aydin <mhaydin@gmail.com>
2019-02-26 07:56:40 +0300

    Count Page Created

--- weekly.php
+++ weekly.php
@@ -1154,7 +1154,7 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
          $.getJSON("../../tcount.php", function(jd) {
              //console.log("jd",jd);
              var datax = jd.map(function(e) {return e.Tarih;});
-             console.log("Tarih",datax);
+             console.log("TarihGoogle",datax);
              var datay = jd.map(function(e) {return e.Giris; });
          });
      };
