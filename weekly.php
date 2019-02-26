419a5ea466ef7db3b6cea64bd1c909b5cb0f77c5
mraydin <mhaydin@gmail.com>
2019-02-26 18:08:50 +0300

    Week Graph Customize

--- weekly.php
+++ weekly.php
@@ -1151,8 +1151,6 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
 
         <script>
 
-
-
             console.log("GetValue", getValueOn().datax);
             var barChartData = {
                 labels: [
@@ -1174,28 +1172,28 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
                     },
                     {
                         label: "11:00",
-                        backgroundColor: "#34495E",
+                        backgroundColor: "#26B99A",
                         borderColor: "blue",
                         borderWidth: 1,
                         data: getValueOnbir().datax
                     },
                     {
                         label: "12:00",
-                        backgroundColor: "#34495E",
+                        backgroundColor: "#ACADAC",
                         borderColor: "blue",
                         borderWidth: 1,
                         data: getValueOniki().datax
                     },
                     {
                         label: "13:00",
-                        backgroundColor: "#34495E",
+                        backgroundColor: "#536c86",
                         borderColor: "blue",
                         borderWidth: 1,
                         data: getValueOnuc().datax
                     },
                     {
                         label: "14:00",
-                        backgroundColor: "#34495E",
+                        backgroundColor: "#3498DB",
                         borderColor: "blue",
                         borderWidth: 1,
                         data: getValueOndort().datax
