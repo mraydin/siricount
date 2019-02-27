4f5f1e643d08b15d381817d5b0d659588d5c21c6
mraydin <mhaydin@gmail.com>
2019-02-27 16:39:28 +0300

    daily.php has been created

--- daily.php
+++ daily.php
@@ -152,75 +152,6 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
 	</div>
               </div>
 
-
-    <div class="row">
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <div class="x_panel">
-                <div class="x_title">
-			<div class="col-md-4">
-                  <h2>Haftalık Trend<small></small></h2></div>
-                  <div class="clearfix"></div>
-                </div>
-                <div class="x_content">
-                  <canvas id="lineChart"></canvas>
-                </div>
-              </div>
-            </div>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <div class="x_panel">
-                <div class="x_content">
-                    <table class="table table-hover">
-                        <thead>
-                        <tr>
-                            <th><b id="weekName"></b>.Hafta</th>
-                            <th>Bu Yıl</th>
-                            <th>Geçen Yıl</th>
-                            <th>Değişim</th>
-                        </tr>
-                        </thead>
-                        <tbody id="theWeek">
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <td>Toplam</td>
-                            <td><b id="totalWeek"></b></td>
-                            <td>--</td>
-                            <td>%-</td>
-                        </tr>
-                        </tfoot>
-                    </table>
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-                <div class="row">
-                    <div class="col-md-12 col-sm-8 col-xs-12">
-                        <div class="x_panel">
-                            <div class="x_title">
-                                <h2>Son 7 gün (<?php  $objDateTime = new DateTime('NOW');
-                                    echo $objDateTime->format('d.m')-6;
-                                    ?> - <?php  $objDateTime = new DateTime('NOW');
-                                    echo $objDateTime->format('d.m');
-                                    ?>)</h2>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="x_content" style="position: relative; margin: auto; height: 40vh; width: 80vw;">
-
-                                <canvas id="MYcanvas"> </canvas>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-
-
-
-
-
                 <div class="row">
             <div class="col-md-12 col-sm-8 col-xs-12">
               <div class="x_panel">
@@ -239,54 +170,6 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
     </div>
           <div class="clearfix"></div>
 
-
-	 <div class="row">
-            <div class="col-md-12 col-sm-8 col-xs-12">
-              <div class="x_panel">
-                <div class="x_title">
-                  <h2>Aylık Trend</h2>
-                  <ul class="nav navbar-right panel_toolbox">
-                    <li><a href="#"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" 
-			aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-close"></i></a>
-                    </li>
-                  </ul>
-                  <div class="clearfix"></div>
-                </div>
-                <div class="x_content">
-
-                  <div id="mainb" style="height:350px;"></div>
-
-                </div>
-              </div>
-            </div>
-
-	 </div>
-
-
-                <div class="row">
-                    <br />
-
-                    <div class="col-md-12 col-sm-8 col-xs-12">
-                        <div class="x_panel tile fixed_height_320 overflow_hidden">
-                            <div class="x_title">
-                                <h2>Haftalık Trend</h2>
-
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="x_content">
-
-                                <div id="myfirstchart" style="width: 20%; height: 220px"></div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
 
 
 
