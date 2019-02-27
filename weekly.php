8b30eecdbfaee74b0694812ef25929e1002d40a0
mraydin <mhaydin@gmail.com>
2019-02-27 14:44:06 +0300

    Line Chart Edit

--- weekly.php
+++ weekly.php
@@ -152,6 +152,48 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
               </div>
 
 
+    <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <div class="x_panel">
+                <div class="x_title">
+			<div class="col-md-4">
+                  <h2>Haftalık Trend<small></small></h2></div>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <canvas id="lineChart"></canvas>
+                </div>
+              </div>
+            </div>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="x_panel">
+                <div class="x_content">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th><b id="weekName"></b>.Hafta</th>
+                            <th>Bu Yıl</th>
+                            <th>Geçen Yıl</th>
+                            <th>Değişim</th>
+                        </tr>
+                        </thead>
+                        <tbody id="theWeek">
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <td>Toplam</td>
+                            <td><b id="totalWeek"></b></td>
+                            <td>--</td>
+                            <td>%-</td>
+                        </tr>
+                        </tfoot>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
                 <div class="row">
                     <div class="col-md-12 col-sm-8 col-xs-12">
                         <div class="x_panel">
@@ -170,39 +212,7 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
                 </div>
 
 
-                <div class="row">
-                    <br />
 
-                    <div class="col-md-12 col-sm-8 col-xs-12">
-                        <div class="x_panel tile fixed_height_320 overflow_hidden">
-                            <div class="x_title">
-                                <h2>Haftalık Trend</h2>
-                                <ul class="nav navbar-right panel_toolbox">
-                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li><a href="#">Settings 1</a>
-                                            </li>
-                                            <li><a href="#">Settings 2</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                                    </li>
-                                </ul>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="x_content">
-
-                                <div id="myfirstchart" style="width: 20%; height: 220px"></div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
 
 
 
@@ -254,46 +264,25 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
 
 
                 <div class="row">
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="x_panel">
+                    <br />
+
+                    <div class="col-md-12 col-sm-8 col-xs-12">
+                        <div class="x_panel tile fixed_height_320 overflow_hidden">
                             <div class="x_title">
-                                <div class="col-md-4">
-                                    <h2>Haftalık Trend<small></small></h2></div>
+                                <h2>Haftalık Trend</h2>
+
                                 <div class="clearfix"></div>
                             </div>
                             <div class="x_content">
-                                <canvas id="lineChart"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="x_panel">
-                            <div class="x_content">
-                                <table class="table table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th><b id="weekName"></b>.Hafta</th>
-                                        <th>Bu Yıl</th>
-                                        <th>Geçen Yıl</th>
-                                        <th>Değişim</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody id="theWeek">
-                                    </tbody>
-                                    <tfoot>
-                                    <tr>
-                                        <td>Toplam</td>
-                                        <td><b id="totalWeek"></b></td>
-                                        <td>--</td>
-                                        <td>%-</td>
-                                    </tr>
-                                    </tfoot>
-                                </table>
+
+                                <div id="myfirstchart" style="width: 20%; height: 220px"></div>
 
                             </div>
                         </div>
                     </div>
+
                 </div>
+
 
 
 
