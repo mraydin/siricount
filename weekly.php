cc51773099f488b9838c7f84d6a24a64e8cd8d8b
mraydin <mhaydin@gmail.com>
2019-02-27 11:35:58 +0300

    Ziyaretci Yas Oranları bolumu guncellendi

--- weekly.php
+++ weekly.php
@@ -1240,6 +1240,20 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
             var chartOptions = {
                 responsive: true,
                 maintainAspectRatio: false,
+                options: {
+                    plugins: {
+                        datalabels: {
+                            color: 'black',
+                            font: {
+                                weight: 'bold'
+                            },
+                            formatter: function(value) {
+                                return value + '%';
+                            },
+
+                            title: false
+                        }
+                    },
                 legend: {
                     position: "top"
                 },
