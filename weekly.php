aa4a3c1979433254a97da307fd0cb97c5692fd62
mraydin <mhaydin@gmail.com>
2019-02-27 12:03:14 +0300

    Chart.js

--- weekly.php
+++ weekly.php
@@ -1294,7 +1294,7 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
                         font: {
                             weight: 'bold'
                         },formatter: function(value, context) {
-                            return context.chart.data.datasets.label;
+                            return context.chart.data.datasets[context.dataIndex].label;
                         },
 
 
