26f4710e0f38d99154aa3de2044e29cb8e225dc7
mraydin <mhaydin@gmail.com>
2019-02-27 11:06:31 +0300

    Ziyaretci Yas Oranları bolumu guncellendi

--- weekly.php
+++ weekly.php
@@ -1255,7 +1255,7 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
                         },
                         scaleLabel: {
                             display: true,
-                            labelString: 'probability'
+                            labelString: 'Ziyaretçi Sayısı'
 
                         }
                     }]
