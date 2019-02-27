a39c248769c52c7c5c5413394c8e84718673374f
mraydin <mhaydin@gmail.com>
2019-02-27 16:23:17 +0300

    Line Chart Edit

--- weekly.php
+++ weekly.php
@@ -231,7 +231,7 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
             </div>
         <div class="clearfix"></div>
 	      </div>
-              <div class="x_content" style="position: relative; margin: auto; height: 60vh; width: 80vw;">
+              <div class="x_content" style="margin: auto; height: 60vh; width: 80vw;">
                   <canvas id="mybarChart""></canvas>
                 </div>
               </div>
