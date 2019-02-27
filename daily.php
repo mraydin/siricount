0acbd6471643f19606774b4e9feb65420b03df73
mraydin <mhaydin@gmail.com>
2019-02-27 17:56:01 +0300

    daily.php has been created

--- daily.php
+++ daily.php
@@ -155,37 +155,28 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
               </div>
 
 
-                <div class="row top_count" style="margin: 10px 0;">
+                <div class="row top_tiles" style="margin: 10px 0;">
 
-                        <div class="col-md-3 tile_stats_count">
-                            <div class="left"></div>
-                            <div class="right">
+                        <div class="col-md-3 tile">
                             <span>Günlük Toplam</span>
-                            <h3>231</h3>
+                            <b>231</b>
                             <span class="sparkline_two" style="height: 160px;">
                                     <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
                                 </span>
-                            </div>
                         </div>
-                        <div class="col-md-3 tile_stats_count">
-                            <div class="left"></div>
-                            <div class="right">
+                        <div class="col-md-3 tile">
                             <span>Saatlik Toplam</span>
                             <b>231,809</b>
                             <span class="sparkline_two" style="height: 160px;">
                                     <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
                                 </span>
-                            </div>
                         </div>
-                        <div class="col-md-3 tile_stats_count">
-                            <div class="left"></div>
-                            <div class="right">
+                        <div class="col-md-3 tile">
                             <span>Dış Alan</span>
                             <b>------</b>
                             <span class="sparkline_two" style="height: 160px;">
                                     <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
                                 </span>
-                            </div>
                         </div>
                         <div class="col-md-3 tile_stats_count">
                         <div class="left"></div>
