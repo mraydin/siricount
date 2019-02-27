8031976b62ef65a12355089e7730e35cbf4ef86f
mraydin <mhaydin@gmail.com>
2019-02-27 16:22:11 +0300

    Line Chart Edit

--- weekly.php
+++ weekly.php
@@ -663,7 +663,7 @@ picker.endDate.locale('tr').format('DD.MM.YYYY') + " to " + picker.startDate.loc
 
       	var options = {
 		responsive: true,
-                maintainAspectRatio: true,
+                maintainAspectRatio: false,
         	scales: {
           		yAxes: [{
             			ticks: {
