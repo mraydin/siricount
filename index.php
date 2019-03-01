235173de97afc9370439dd486371deac78b494ea
mraydin <mhaydin@gmail.com>
2019-03-01 16:54:29 +0300

    daily.php has been created

--- index.php
+++ index.php
@@ -1038,7 +1038,7 @@ aria-expanded="false"><i class="fa fa-wrench"></i></a>
                 return e.Giris;
                 });
 
-    console.log("Labels :",labels);
+    console.log("Labels :",count);
 
 	// Bar chart
     	var ctx = document.getElementById("mybarChart");
