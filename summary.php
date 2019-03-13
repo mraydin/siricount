8c29fd5a0b96b8ae25325f4b7d2a69f5239192c8
mraydin <mhaydin@gmail.com>
2019-03-13 15:34:43 +0300

    summary.php created

--- summary.php
+++ summary.php
@@ -528,8 +528,8 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
                             text += "<td>" + icon +  (((datay[i] - predatay[i]) / predatay[i]) * 100).toFixed(0) + "%" + "</td></tr>";
                         }
                         text += "</tr>";
-                        document.getElementById("totalWeek").innerHTML = total;
-                        document.getElementById("pretotalWeek").innerHTML = pretotal;
+                        document.getElementById("totalWeek").innerHTML = pretotal;
+                        document.getElementById("pretotalWeek").innerHTML = total;
                         document.getElementById("theWeek").innerHTML = text;
                         document.getElementById("weekName").innerHTML = moment().locale('tr').format('DD.MM.YYYY');
                         document.getElementById("preweekName").innerHTML  = moment().subtract(1, 'days').format('DD.MM.YYYY');
