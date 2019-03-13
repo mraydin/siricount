87909c78b4914e700d305005e9aa4c95aecf0786
mraydin <mhaydin@gmail.com>
2019-03-13 09:14:10 +0300

    summary.php created

--- summary.php
+++ summary.php
@@ -820,8 +820,8 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
                     function updategetMonthValue(chart) {
 
                         //chart.destroy();
-                        var myObject = {name: picker.startDate.locale('tr').format('DD'), s: "submit",
-                        surname: picker.endDate.locale('tr').format('DD')};
+                        var myObject = {name: picker.startDate.locale('tr').format('DD.MM.YYYY'), s: "submit",
+                        surname: picker.endDate.locale('tr').format('DD.MM.YYYY')};
 
                         $.getJSON("../../daycountSearch.php",myObject, function(jd) {
                             //console.log("jd",jd);
