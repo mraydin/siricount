09fefdace8691883c0e93a98af77d1d928ab4616
mraydin <mhaydin@gmail.com>
2019-03-14 17:21:45 +0300

    summary.php created

--- summary.php
+++ summary.php
@@ -1028,7 +1028,7 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
 
 
 
-        <!-- paces -->
+        <!-- pace -->
         <script src="js/pace/pace.min.js"></script>
 
         <script src="js/custom.min.js"></script>
