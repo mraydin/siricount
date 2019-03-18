585b4d17efdccff07dfab8229a885ddaca4801c6
mraydin <mhaydin@gmail.com>
2019-03-18 13:01:13 +0300

    summary.php created

--- daily.php
+++ daily.php
@@ -177,7 +177,7 @@ fa-paw"></i> <span>SiriCount v2.0!</span></a>
                         </div>
                         <div class="col-md-3 tile">
                             <span>Dış Alan</span>
-                            <h2>------</h2>
+                            <h2 id="disAlan"></h2>
                             <span class="sparkline_two" style="height: 160px;">
                                     <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
                                 </span>
