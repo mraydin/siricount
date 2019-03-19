51a9da0a7b2ae94be297b07abde9e5c495e88ce4
mraydin <mhaydin@gmail.com>
2019-03-19 10:17:47 +0300

    font-awesome.min updated

--- inc/head.php
+++ inc/head.php
@@ -16,7 +16,7 @@ echo '<head>
   <link href="css/animate.min.css" rel="stylesheet">
 
   <!-- Custom styling plus plugins -->
-  <link href="css/custom.css" rel="stylesheet">
+  <link href="../production/css/custom.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/maps/jquery-jvectormap-2.0.3.css" />
   <link href="css/icheck/flat/green.css" rel="stylesheet" />
   <link href="css/floatexamples.css" rel="stylesheet" type="text/css" />
